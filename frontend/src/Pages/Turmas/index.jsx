5069cb0a4830b617d92b744ccdaf1c12131ba005
Edilson <edilsonemidio93@gmail.com>
2026-07-07 01:16:50 -0300

    (front): page turmas integred with backend, but missing aditions

--- frontend/src/Pages/Turmas/index.jsx
+++ frontend/src/Pages/Turmas/index.jsx
@@ -1,36 +1,48 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import API from "../../Controller/Api.jsx";
 
 export default function Turmas(){
 
   const navigate = useNavigate();
-  const [turmas, setTurmas] = useState([{nome:"teste"}, {nome:"BABABA"}]);
-  // fazer a integração com o back ainda
-  // puxar as turmas
-  // e fazer a rota de chamadas
+  const [turmas, setTurmas] = useState([]);
+
+  useEffect(()=>{
+
+    API.get("/turmas").then((res)=>{
+      console.log(res.data);
+      setTurmas(res.data);
+    }).catch((err)=>{
+      
+      console.log(err);
+    });
+    //tem que terminar as informações para preencher o card abaixo
+  
+  },[]);
 
   return(
     <div className="ml-10 mt-5">
       <h1 className="text-2xl font-bold">Turmas</h1>
       <p>Todas as turmas</p>
 
-      <div className="flex gap-5 mt-5 grid-cols-3">
+      <div className="gap-5 mt-5 grid grid-cols-3 mr-10">
         {
           turmas.map((e,index)=>(
-            <div key={index} className="border rounded-2xl min-w-70 border-gray-400">
+            <div key={index} className="border rounded-2xl min-w-60 border-gray-400">
               <div className="bg-blue-100 p-5 rounded-t-2xl">
                 <p className="text-sm">Manhã - Prof. {e.nome}</p>
-                <h2 className="text-xl font-bold">1º Ano A</h2>
+                <h2 className="text-xl font-bold">{e.serie} {e.nome}</h2>
               </div>
               <div className="p-5 rounded-2xl">
                 <div className="flex justify-between mb-5">
                   <span className="text-sm flex">
                   <img src="icons/user.svg" alt="" className="w-5 mr-1"/>
-                    30
+                    {e._count.alunos}
                   </span>
                   <span className="bg-amber-200 rounded-xl px-2 text-sm">90% presença</span>
                 </div>
-                <button className="bg-blue-600 hover:bg-blue-400 text-white p-2 w-full rounded-2xl font-bold" onClick={()=>navigate("/")}>Abrir chamada</button>
+                <button className="bg-blue-600 hover:bg-blue-400 text-white p-2 w-full rounded-2xl font-bold" onClick={()=>navigate(`/attendance?id=${e.id}`)}>Abrir chamada</button>
               </div>
             </div>
           ))
