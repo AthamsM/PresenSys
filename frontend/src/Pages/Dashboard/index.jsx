69038b2dafc27e98449a17dd910c4c46b39beca6
Manoel Filho <nogueirafilho888@gmail.com>
2026-08-19 09:15:02 -0300

    feat(front): add chart fouls per month

--- frontend/src/Pages/Dashboard/index.jsx
+++ frontend/src/Pages/Dashboard/index.jsx
@@ -3,23 +3,30 @@ import Card from "../../Components/Card";
 import { useEffect, useState } from "react";
 import API from "../../Controller/Api";
 import { Bar } from "react-chartjs-2";
+import { Line } from 'react-chartjs-2';
 import {
   Chart as ChartJS,
   CategoryScale,
   LinearScale,
   BarElement,
+  PointElement,
+  LineElement,
   Title,
   Tooltip,
   Legend,
+  Filler,
 } from "chart.js";
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
+  PointElement,
+  LineElement,
   BarElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  Filler,
 );
 
 export default function Dashboard(){
@@ -27,9 +34,35 @@ export default function Dashboard(){
   const [classes, setClasses] = useState([]);
   const [totalStudents, setTotalStudents] = useState(0);
   const [totalFouls, setTotalFouls] = useState(0);
+  const cards = [
+
+    {
+      "title":"Turmas",
+      "value":classes.length,
+      "image":"icons/class-blue.svg",
+    },
+    {
+      "title":"Alunos",
+      "value":totalStudents,
+      "image":"icons/user-blue.svg",
+    },
+    {
+      "title":"Faltas do mês",
+      "value":totalFouls,
+      "image":"icons/calendar-blue.svg",
+    },
+    {
+      "title":"Frequência média",
+      "value":totalFouls==0 ? "100%" : ((100-(totalFouls/(totalStudents*getPreviousBusinessDays()))*100)).toFixed(1)+"%",
+      "image":"icons/trend-up-blue.svg",
+    }
+  ];
 
   const year = new Date().getFullYear();
   const [studentsMostFouls, setStudentsMostFouls] = useState([]);
+  const [foulsPerMonth, setFoulsPerMonth] = useState([]);
+  const labelsMonth = ["jan", "fev", "mar", "abr", "maio", "jun", "jul", "ago", "set", "out", "nov", "dez"];
+  const labelsMonthFull = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
   const calculateTotalStudents = (data)=>{
 
@@ -51,30 +84,6 @@ export default function Dashboard(){
     });
     setTotalFouls(total);
   } 
-  
-  const cards = [
-
-    {
-      "title":"Turmas",
-      "value":classes.length,
-      "image":"icons/class.svg",
-    },
-    {
-      "title":"Alunos",
-      "value":totalStudents,
-      "image":"icons/user.svg",
-    },
-    {
-      "title":"Faltas do mês",
-      "value":totalFouls,
-      "image":"icons/calendar.svg",
-    },
-    {
-      "title":"Frequência média",
-      "value":totalFouls==0 ? "100%" : ((100-(totalFouls/(totalStudents*getPreviousBusinessDays()))*100)).toFixed(1)+"%",
-      "image":"icons/trend-up.svg",
-    }
-  ];
   
   function getPreviousBusinessDays() {
 
@@ -142,15 +151,29 @@ export default function Dashboard(){
 
   },[]);
 
+  useEffect(() => {
+
+    API.get(`/charts/fouls-per-month/${year}`).then(response=>{
+      
+      setFoulsPerMonth(response.data);
+
+    }).catch(error=>{
+
+      console.log(error);
+
+    });
+
+  },[]);
+
   return(
 
     <div className="grid gap-y-[2rem]">
       
-      <div className="grid gap-y-[1rem]">
+      <div className="grid gap-y-[1rem] divide-y-[0.138rem] divide-[#99A1AF]/10">
 
         <h1 className="text-2xl font-bold text-[#023580]">Visão geral das turmas durante o mês</h1>
         
-        <div className="flex justify-around gap-x-[1rem] w-full overflow-y-hidden overflow-x-scroll md:overflow-hidden">
+        <div className="flex justify-around gap-x-[1rem] text-[#263238]/90 w-full overflow-y-hidden overflow-x-scroll md:overflow-hidden">
           {
             cards.map((e, index)=>(<Card image={e.image} title={e.title} value={e.value} key={index} className="bg-[#DBEAFE]/50 rounded-lg flex-none md:flex [--animation-duration:0.33s] animate-scale-in-center "/>))
           }
@@ -158,25 +181,63 @@ export default function Dashboard(){
 
       </div>
 
-      <div className="grid gap-y-[1rem]"> 
+      <div className="grid gap-y-[1rem] divide-y-[0.138rem] divide-[#99A1AF]/10"> 
 
         <h1 className="text-2xl font-bold text-[#023580]">Visão geral das turmas durante o ano</h1>
         
-        <div className="grid grid-cols-2">
+        <div className="grid grid-cols-2 gap-[2rem]">
 
           <div className=" flex flex-col justify-start items-start bg-[#DBEAFE]/50 rounded-lg divide-y-[0.138rem] divide-[#99A1AF]/80"> 
 
-            <h1 className="p-[0.5rem] w-full text-center text-xl font-bold text-[#263238]">Alunos com mais faltas durante o ano</h1>
+            <h1 className="p-[0.5rem] w-full text-center text-[1.1rem] font-bold text-[#263238]/90">Faltas durante os meses do ano</h1>
+
+            {foulsPerMonth &&
+
+              <Line className="p-[1rem]"
+                options= {{
+                  responsive: true, 
+                  maintainAspectRatio: true, 
+                  plugins: {
+                    legend: {display: false}, 
+                    tooltip: {titleFont: {size: 15, weight: "bold"}, bodyFont: {size: 14, weight: "bold"}, callbacks: {title: (con) => {return labelsMonthFull[foulsPerMonth[con[0].dataIndex].mnt - 1]}}}
+                  }, 
+                  scales: { 
+                    x: { beginAtZero: true, ticks: {precision: 0, color: "#263238", font: {weight: "bold"}}, grid: {display: true, color: "#99A1AF"}}, 
+                    y: { ticks: {autoSkip: false, color: "#263238", font: {weight: "bold"}}, grid: {display: true, color: "#99A1AF"}},
+                  }
+                }}
+                data={{
+                  labels: foulsPerMonth.map(fouls => labelsMonth[fouls.mnt - 1]),
+                  datasets: [{
+                    fill: true,
+                    label: "Faltas",
+                    data: foulsPerMonth.map(fouls => fouls.fouls),
+                    backgroundColor: "#155DDD90",
+                    borderColor: "#155DDD",
+                    borderRadius: 6,
+                    barThickness: 24,
+                  },]
+                }}
+              />
+            }
+
+          </div>
+
+          <div className="flex flex-col justify-start items-start bg-[#DBEAFE]/50 rounded-lg divide-y-[0.138rem] divide-[#99A1AF]/80"> 
+
+            <h1 className="p-[0.5rem] w-full text-center text-[1.1rem] font-bold text-[#263238]/90">Alunos com mais faltas durante o ano</h1>
 
             {studentsMostFouls &&
 
-              <Bar className="p-[0.5rem]"
+              <Bar className="p-[1rem]"
                 options= {{
                   indexAxis: "y",
                   responsive: true, 
                   maintainAspectRatio: true, 
-                  plugins: { legend: {display: false}, 
-                  tooltip: {callbacks: {title: (con) => {return studentsMostFouls[con[0].dataIndex].student.name}}}}, 
+                  plugins: {
+                    legend: {display: false}, 
+                    tooltip: {titleFont: {size: 15, weight: "bold"}, bodyFont: {size: 14, weight: "bold"}, callbacks: {title: (con) => {return studentsMostFouls[con[0].dataIndex].student.name}}}
+                  }, 
                   scales: { 
                     x: { beginAtZero: true, ticks: {precision: 0, color: "#263238", font: {weight: "bold"}}, grid: {display: true, color: "#99A1AF"}}, 
                     y: { ticks: {autoSkip: false, color: "#263238", font: {weight: "bold"}}, grid: {display: false}}
@@ -187,10 +248,12 @@ export default function Dashboard(){
                   datasets: [{
                     label: "Faltas",
                     data: studentsMostFouls.map(mostFouls => mostFouls.fouls),
-                    backgroundColor: "#155DDD",
+                    backgroundColor: "#155DDDEA",
                     borderRadius: 6,
-                    barThickness: 24,
+//                    barThickness: 16,
                     borderWidth: 0,
+                    barPercentage: 0.8,
+                    categoryPercentage: 1,
                   },]
                 }}
               />
@@ -200,9 +263,7 @@ export default function Dashboard(){
 
         </div>
 
-
       </div>
-
 
     </div>
 
