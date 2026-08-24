63650dcd4b7756f083e011055ab8bba3840df8db
Maria Verônica <mv9261654@gmail.com>
2026-08-24 17:08:15 -0300

    Refactor(back): Creation of a field to justify absences and changes to the Prisma table

--- backend/src/controllers/attendanceController.js
+++ backend/src/controllers/attendanceController.js
@@ -3,8 +3,8 @@ import AttendanceService from '../services/attendanceService.js';
 class AttendanceController {
   async register(req, res, next) {
     try {
-      const { date, attendance } = req.body;
-      const result = await AttendanceService.register(date, attendance);
+      const { classId, date, attendance } = req.body;
+      const result = await AttendanceService.register(classId, date, attendance);
       return res.status(200).json({ message: 'Chamada registrada com sucesso!', quantidade: result.length });
     } catch (error) {
       next(error);
