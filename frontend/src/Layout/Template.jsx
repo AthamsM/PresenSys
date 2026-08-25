8767666a9fe3f787b0b86b5a8e2e0cba7f36c3db
Edilson <edilsonemidio93@gmail.com>
2026-08-25 14:25:46 -0300

    fix(front): sidebar now is pined on mobile devices and ever open in desktop devices

--- frontend/src/Layout/Template.jsx
+++ frontend/src/Layout/Template.jsx
@@ -1,11 +1,33 @@
 import { useState } from "react";
 import Navbar from "../Components/Navbar";
 import Sidebar from "../Components/Sidebar";
+import { useEffect } from "react";
 
 function Template( {children} ){
 
-  const [activateSide, setActivateSide] = useState(false)
+  const [activateSide, setActivateSide] = useState(true)
   
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if(width <= 640){
+        setActivateSide(false);
+      }else{
+        setActivateSide(true);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Call the handler once to set the initial state
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   const changeSide = () => {
     setActivateSide(!activateSide);
     const width = window.innerWidth;
@@ -26,13 +48,13 @@ function Template( {children} ){
     
   }
   return(
-      <div className="h-screen bg-[#F9FBFC] flex grid-cols-2">
+      <div className="min-h-screen bg-[#F9FBFC] flex grid-cols-2">
 
         {activateSide && <Sidebar/> }
         
-        <div className="flex-1" onClick={(e)=>removeSide(e.target.id)}>
+        <div className="flex-1 relative" onClick={(e)=>removeSide(e.target.id)}>
           { backgroundGray &&
-            <div className="absolute h-screen w-full opacity-50 bg-gray-700 z-1"></div>
+            <div className="absolute inset-0 flex w-full opacity-50 bg-gray-700 z-1"></div>
           } 
           <Navbar alter={changeSide}/>
           <div className="m-5">
